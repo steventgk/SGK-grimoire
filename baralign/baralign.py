767f5aa633551b7187c470122f3ce780bd5c8049
Steven Gough-Kelly <steventgk@gmail.com>
2022-09-18 21:49:01 +0100

    add distmod+baralign
    add folders, notebook examples and python importable scripts for distance modulus and bar alignment codes.

---
+++ baralign/baralign.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pynbody as pb
+
+def bar_align(galaxy, rbar, barfrac = 0.5, zlim=0.5, log=False):
+    """
+    Aligns the bar of pynbody galaxy simulation with the x-axis assuming the
+    galaxy disc is already aligned to the XY plane using the inertial tensor.
+
+    Function does not return any values/object. Pynbody functions effect the
+    global variable which stores 'galaxy' so rotations within the functions
+    are applied to input variable 'galaxy'.
+
+    Parameters
+    ----------
+    galaxy : pynbody simulation object
+        Galaxy object in the XY plane to be aligned.
+
+    rbar : float
+        Bar radius in simulation units e.g. kpc.
+
+    barfrac : float
+        Fraction of bar length to calculate the inertial tensor within in
+        simulation units e.g. kpc.
+
+    zlim : float
+        Vertical limit to calculate intertial tensor within in simulation units
+        e.g. kpc. Useful in galaxies with thick discs and weak bars.
+
+    log : Bool
+        Flag to output print statements.
+
+    Returns
+    -------
+    None
+
+    """
+    if np.isnan(rbar):
+        if log:
+            print('* Bar undefined, using 1 kpc *')
+        rbar = 1.0
+    elif rbar*barfrac < 1.:
+        rbar = 1
+        if log:
+            print('* Short Bar, using 1 kpc *')
+    else:
+        rbar = rbar*barfrac
+        if log:
+            print('* Bar defined, aligning to {} kpc *'.format(rbar))
+
+    if log:
+        print('* Realigning bar using |z| < {} *'.format(zlim))
+
+    zfilt = pb.filt.LowPass('z',zlim)&pb.filt.HighPass('z',-zlim)
+    rfilt = pb.filt.LowPass('rxy',rbar)
+
+    x = np.array(galaxy[zfilt&rfilt].star['pos'].in_units('kpc'))[:,0]
+    y = np.array(galaxy[zfilt&rfilt].star['pos'].in_units('kpc'))[:,1]
+    m = np.array(galaxy.star[zfilt&rfilt]['mass'])
+
+    #Calculate the inertia tensor
+    I_yy, I_xx, I_xy = np.sum(m*y**2),np.sum(m*x**2),np.sum(m*x*y)
+    I = np.array([[I_yy, -I_xy], [-I_xy, I_xx]])
+
+    #Calculate the eigenvalues and eigenvectors
+    eigenvalues, eigenvectors = np.linalg.eig(I)
+    lowest = eigenvalues.argmin()
+    maj_axis = eigenvectors[:, lowest]
+
+    #Get the angle we need to rotate by
+    r_angle = np.degrees(np.arctan2(maj_axis[1], maj_axis[0]))
+
+    galaxy.rotate_z(-r_angle)
+
+    if log:
+        print('* Bar realigned by {} degrees*'.format(r_angle))
+
+    return None
---
+++ distmod/distmod.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+def distmod(app_mag=None,abs_mag=None,dist=None,extinc=None,output=None):
+    """
+    Conversions through distance modulus equation for apparent, absolute
+    and distance parameters with optional reddening.
+
+    Parameters
+    ----------
+    app_mag : float or ndarray of float
+        apparent magnitude(s) of source(s) in mag.
+
+    abs_mag : float or ndarray of float
+        absolute magnitude(s) of source(s) in mag.
+
+    dist : float or ndarray of float
+        distances(s) of source(s) in kpc.
+
+    extinc : float or ndarray of float
+        extinction value(s) in relevant band of source(s) in mag.
+
+    output : str
+        changes appropriate output. Also changes required inputs.
+        can take values:
+            * 'abs'    : calculate the absolute magnitude.
+                         Requires app_mag and dist.
+            * 'app'    : calculate the apparent magnitude.
+                         Requires abs_mag and dist.
+            * 'dist'   : calculate the distance.
+                         Requires app_mag and dist.
+            * 'extinc' : calculate the extinction.
+                         Requires app_mag, abs_mag and dist.
+
+    Returns
+    -------
+    calculation: float or ndarray of float
+        dependent on the output parameter.
+    """
+
+    def app2abs(app_mag=None,dist=None,extinc=None):
+        if type(extinc) is type(None):
+            extinc = np.zeros(len(app_mag))
+        return app_mag - extinc - 5*(np.log10(dist)+2)
+
+    def abs2app(abs_mag=None,dist=None,extinc=None):
+        if type(extinc) is type(None):
+            extinc = np.zeros(len(abs_mag))
+        return abs_mag + extinc + 5*(np.log10(dist)+2)
+
+    def mag2dist(app_mag=None,abs_mag=None,extinc=None):
+        if type(extinc) is type(None):
+            extinc = np.zeros(len(app_mag))
+        return 10**(((app_mag - abs_mag - extinc)/5)-2)
+
+    def magdist2extinc(app_mag=None,abs_mag=None,dist=None):
+        return app_mag - 5*(np.log10(dist)+2) - abs_mag
+
+    if output == 'abs':
+            return app2abs(app_mag=app_mag,dist=dist,extinc=extinc)
+    elif output=='app':
+            return abs2app(abs_mag=abs_mag,dist=dist,extinc=extinc)
+    elif output=='dist':
+            return mag2dist(app_mag=app_mag,abs_mag=abs_mag,extinc=extinc)
+    elif output=='extinc':
+            return magdist2extinc(app_mag=app_mag,abs_mag=abs_mag,dist=dist)
+
+    else:
+        return print('output type not found: '+ \
+                   'Options "abs","app","dist" and "extinc"')
